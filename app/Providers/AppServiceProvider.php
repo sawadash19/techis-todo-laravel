9743e104bc0d34f0ceb00dd2544e21ce12e22f86
sawadash19 <shu19951019@icloud.com>
2022-03-23 17:50:58 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (\App::environment(['production']) || \App::environment(['develop']) {
+        if (\App::environment(['production']) || \App::environment(['develop'])) {
             \URL::forceScheme('https');
         }
     }
